091180164a177085b78a7ce4032e09ca400b8d5e
Mekonen-Abera <makoninabera@gmail.com>
2024-02-26 01:01:31 -0800

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-# Installs flask
+# Puppet script that installs flask
+
+package { 'python3-pip':
+  ensure => 'installed',
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
